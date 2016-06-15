2c277331818d354d6151f1f8178aee1023e0e465
takayanagi <takayanagi@192.168.11.4>
2016-06-15 22:53:15 +0900

    問合せツール製造途中。

--- HTML/META-INF/resources/dataforms/devtool/field/common/FunctionSelectField.js
+++ HTML/META-INF/resources/dataforms/devtool/field/common/FunctionSelectField.js
@@ -20,16 +20,26 @@ FunctionSelectField = createSubclass("FunctionSelectField", {}, "SingleSelectFie
 FunctionSelectField.prototype.attach = function() {
 	SingleSelectField.prototype.attach.call(this);
 	var thisField = this;
-	var thisForm = this.getParentForm();
 	this.get().change(function() {
-		var funcname = $(this).val();
-		if (funcname != null && funcname.length > 0) {
-			var packageName = funcname.replace(/\//g, ".").substr(1);
-			if (thisField.packageOption.length > 0) {
-				packageName +=  "." + thisField.packageOption;
-			}
-			thisForm.find("#" + thisField.selectorEscape(thisField.packageFieldId)).val(packageName);
-		}
+		thisField.setPackageName($(this))
 	});
 }
 
+FunctionSelectField.prototype.setPackageName = function(jq) {
+	var form = this.getParentForm();
+	var funcname = jq.val();
+	if (funcname != null && funcname.length > 0) {
+		var packageName = funcname.replace(/\//g, ".").substr(1);
+		if (this.packageOption.length > 0) {
+			packageName +=  "." + this.packageOption;
+		}
+		var id = jq.attr("id");
+		logger.log("functionSelectField id=" + id)
+		if (this.isHtmlTableElementId(id)) {
+			var a = id.split(".");
+			form.find("#" + this.selectorEscape(a[0] + "." + this.packageFieldId)).val(packageName);
+		} else {
+			form.find("#" + this.selectorEscape(this.packageFieldId)).val(packageName);
+		}
+	}
+};
---
+++ src/dataforms/devtool/page/query/QueryPage.java
@@ -0,0 +1,52 @@
+package dataforms.devtool.page.query;
+
+import dataforms.app.page.base.BasePage;
+import dataforms.dao.Dao;
+import dataforms.dao.Table;
+
+
+/**
+ * ページクラス。
+ */
+public class QueryPage extends BasePage {
+	/**
+	 * コンストラクタ。
+	 */
+	public QueryPage() {
+		this.addForm(new QueryQueryForm());
+		this.addForm(new QueryQueryResultForm());
+
+	}
+
+	/**
+	 * Pageが配置されるパスを返します。
+	 *
+	 * @return Pageが配置されるパス。
+	 */
+	public String getFunctionPath() {
+		return "/dataforms/devtool";
+	}
+
+	/**
+	 * 操作対象テーブルクラスを取得します。
+	 * <pre>
+	 * ページjavaクラス作成で用のメソッドです。
+	 * </pre>
+	 * @return 操作対象テーブル。
+	 */
+	public Class<? extends  Table> getTableClass() {
+		return Table.class;
+	}
+
+	/**
+	 * テーブルを操作するためのDaoクラスを取得します。
+	 * <pre>
+	 * ページjavaクラス作成で用のメソッドです。
+	 * </pre>
+	 * @return Daoクラス。
+	 */
+	public Class<? extends Dao> getDaoClass() {
+		return Dao.class;
+	}
+
+}
