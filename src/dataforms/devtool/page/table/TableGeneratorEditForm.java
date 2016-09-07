edd8cfc79b523e75c67da0ef70172efea5867c1f
takayanagi <takayanagi@takayanagi-mc>
2016-09-07 09:01:02 +0900

    テーブルjavaソース作成ツールを改善。

--- src/dataforms/devtool/page/table/TableGeneratorEditForm.java
+++ src/dataforms/devtool/page/table/TableGeneratorEditForm.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +32,15 @@ import dataforms.field.common.CreateUserIdField;
 import dataforms.field.common.FlagField;
 import dataforms.field.common.UpdateTimestampField;
 import dataforms.field.common.UpdateUserIdField;
+import dataforms.field.sqltype.BigintField;
+import dataforms.field.sqltype.CharField;
+import dataforms.field.sqltype.ClobField;
+import dataforms.field.sqltype.DateField;
+import dataforms.field.sqltype.IntegerField;
+import dataforms.field.sqltype.NumericField;
+import dataforms.field.sqltype.SmallintField;
+import dataforms.field.sqltype.TimeField;
+import dataforms.field.sqltype.TimestampField;
 import dataforms.field.sqltype.VarcharField;
 import dataforms.servlet.DataFormsServlet;
 import dataforms.util.ClassNameUtil;
@@ -486,6 +496,144 @@ public class TableGeneratorEditForm extends EditForm {
 	@Override
 	protected void insertData(final Map<String, Object> data) throws Exception {
 		log.debug("data=" + JSON.encode(data, true));
+		this.writeTableJavaSource(data);
+	}
+
+	/**
+	 * フィールドIDを取得します。
+	 * @param m フィールドリスト。
+	 * @return フィールドID。
+	 */
+	private String getFieldId(final Map<String, Object> m) {
+		String fieldId = (String) m.get("fieldId");
+		if (StringUtil.isBlank(fieldId)) {
+			String fclass = (String) m.get("fieldClassName");
+			StringBuilder fsb = new StringBuilder(fclass.replaceAll("Field$", ""));
+			char c = fsb.charAt(0);
+			fsb.setCharAt(0, Character.toLowerCase(c));
+			fieldId = fsb.toString();
+		}
+		return fieldId;
+	}
+	
+	
+	/**
+	 * フィールドIdの定数を展開します。
+	 * @param list フィールドリスト。
+	 * @return フィールドIDの定数値。
+	 */
+	private String generateFieldIdConstant(final List<Map<String, Object>> list) {
+		StringBuilder sb = new StringBuilder();
+		for (Map<String, Object> m: list) {
+			String fieldId = getFieldId(m);
+			String comment = (String) m.get("comment");
+			sb.append("\t\t/** " + comment + "のフィールドID。 */\n");
+			sb.append("\t\tpublic static final String ID_");
+			sb.append(StringUtil.camelToSnake(fieldId).toUpperCase());
+			sb.append(" = \"");
+			sb.append(fieldId);
+			sb.append("\";\n");
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * フィールドに対応する値の型を返します。
+	 * @param field フィールド。
+	 * @return 値の型。
+	 */
+	private Class<?> getFieldValueType(final Class<?> field) {
+		Class<?> ret = Object.class;
+		if (CharField.class.isAssignableFrom(field)
+			|| VarcharField.class.isAssignableFrom(field)
+			|| ClobField.class.isAssignableFrom(field)) {
+			ret = String.class;
+		} else if (DateField.class.isAssignableFrom(field)) {
+			ret = java.sql.Date.class;
+		} else if (TimeField.class.isAssignableFrom(field)) {
+			ret = java.sql.Time.class;
+		} else if (TimestampField.class.isAssignableFrom(field)) {
+			ret = java.sql.Timestamp.class;
+		} else if (SmallintField.class.isAssignableFrom(field)) {
+			ret = Short.class;
+		} else if (IntegerField.class.isAssignableFrom(field)) {
+			ret = Integer.class;
+		} else if (BigintField.class.isAssignableFrom(field)) {
+			ret = Long.class;
+		} else if (NumericField.class.isAssignableFrom(field)) {
+			ret = BigDecimal.class;
+		}
+		return ret;
+	}
+
+	/**
+	 * フィールドIdの定数を展開します。
+	 * @param list フィールドリスト。
+	 * @return フィールドIDの定数値。
+	 * @throws Exception 例外。
+	 */
+	private String generateFieldValueGetterSetter(final List<Map<String, Object>> list) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		for (Map<String, Object> m: list) {
+			String superPackageName = (String) m.get("superPackageName");
+			String superSimpleClassName = (String) m.get("superSimpleClassName");
+			Class<?> cls = Class.forName(superPackageName + "." + superSimpleClassName);
+			Class<?> valueType = this.getFieldValueType(cls);
+			log.debug(superSimpleClassName + ":" + valueType);
+			String fieldId = getFieldId(m);
+			String uFieldId = fieldId.substring(0, 1).toUpperCase() + fieldId.substring(1);
+//			String fieldClassSimpleName = (String) m.get("fieldClassName");
+			String comment = (String) m.get("comment");
+			sb.append("\t\t/**\n");
+			sb.append("\t\t * " + comment + "を取得します。\n");
+			sb.append("\t\t * @return " + comment + "。\n");
+			sb.append("\t\t */\n");
+			sb.append("\t\tpublic " + valueType.getName() + " get" + uFieldId+ "() {\n");
+			sb.append("\t\t\treturn (" + valueType.getName() + ") this.getMap().get(Entity.ID_" + StringUtil.camelToSnake(fieldId).toUpperCase() + ");\n");
+			sb.append("\t\t}\n\n");
+
+			sb.append("\t\t/**\n");
+			sb.append("\t\t * " + comment + "を設定します。\n");
+			sb.append("\t\t * @param " + fieldId + " " + comment + "。\n");
+			sb.append("\t\t */\n");
+			sb.append("\t\tpublic void set" + uFieldId+ "(final " + valueType.getName() + " " + fieldId + ") {\n");
+			sb.append("\t\t\tthis.getMap().put(Entity.ID_" + StringUtil.camelToSnake(fieldId).toUpperCase() + ", " + fieldId + ");\n");
+			sb.append("\t\t}\n\n");
+}
+		return sb.toString();
+	}
+
+	
+	/**
+	 * フィールドIdの定数を展開します。
+	 * @param list フィールドリスト。
+	 * @return フィールドIDの定数値。
+	 */
+	private String generateFieldGetter(final List<Map<String, Object>> list) {
+		StringBuilder sb = new StringBuilder();
+		for (Map<String, Object> m: list) {
+			String fieldId = getFieldId(m);
+			String fieldClassSimpleName = (String) m.get("fieldClassName");
+			String comment = (String) m.get("comment");
+			sb.append("\t/**\n");
+			sb.append("\t * " + comment + "フィールドを取得します。\n");
+			sb.append("\t * @return " + comment + "フィールド。\n");
+			sb.append("\t */\n");
+			sb.append("\tpublic " + fieldClassSimpleName + " get" + fieldClassSimpleName + "() {\n");
+			sb.append("\t\treturn (" + fieldClassSimpleName + ") this.getField(Entity.ID_" + StringUtil.camelToSnake(fieldId).toUpperCase() + ");\n");
+			sb.append("\t}\n\n");
+		}
+		return sb.toString();
+	}
+
+	
+	
+	/**
+	 * Table.javaのソースを作成します。
+	 * @param data POSTされたデータ。
+	 * @throws Exception 例外。
+	 */
+	public void writeTableJavaSource(final Map<String, Object> data) throws Exception {
 		String tsrc = this.getTemplate("template/Table.java.template");
 		String tableOverwriteMode = (String) data.get("overwriteMode");
 		String packageName = (String) data.get("packageName");
@@ -545,15 +693,41 @@ public class TableGeneratorEditForm extends EditForm {
 		}
 		tsrc = tsrc.replaceAll("\\$\\{importList\\}", implist.toString());
 		tsrc = tsrc.replaceAll("\\$\\{constructor\\}", constructor.toString());
+		tsrc = tsrc.replaceAll("\\$\\{idConstants\\}", this.generateFieldIdConstant(fieldList));
+		tsrc = tsrc.replaceAll("\\$\\{valueGetterSetter\\}", this.generateFieldValueGetterSetter(fieldList));
+		tsrc = tsrc.replaceAll("\\$\\{fieldGetter\\}", this.generateFieldGetter(fieldList));
 		log.debug("tsrc=\n" + tsrc);
 		if (!OverwriteModeField.SKIP.equals(tableOverwriteMode)) {
 			srcmap.put(packageName + "." + tableClassName, tsrc);
 		}
-		log.debug("srcmap=" + JSON.encode(srcmap));
+		String rclass = packageName + "." + tableClassName + "Relation";
+
 		String path = (String) data.get("javaSourcePath");
+		File relationFile = new File(path + "/" + rclass.replaceAll("\\.", "/") + ".java");
+		if (!relationFile.exists()) {
+			srcmap.put(rclass, this.generateRelationJavaSource(data));
+		}
+		log.debug("srcmap=" + JSON.encode(srcmap));
 		this.writeJavaSource(path, srcmap);
 	}
 
+	
+	/**
+	 * Relation.javaのソースを作成します。
+	 * @param data データ。
+	 * @return Relation.javaのソース。
+	 * @throws Exception 例外。
+	 */
+	private String generateRelationJavaSource(final Map<String, Object> data) throws Exception {
+		String rsrc = this.getTemplate("template/Relation.java.template");
+		String packageName = (String) data.get("packageName");
+		String tableClassName = (String) data.get("tableClassName");
+		rsrc = rsrc.replaceAll("\\$\\{packageName\\}", packageName);
+		rsrc = rsrc.replaceAll("\\$\\{TableClassShortName\\}", tableClassName);
+		return rsrc;
+	}
+
+	
 	/**
 	 * javaソース出力。
 	 * @param path javaソースパス。
