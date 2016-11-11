e76f1f0c8edf58cd687d3ece52f4e8e91b257ba1
takayanagi <takayanagi@takayanagi-mc>
2016-11-11 16:48:08 +0900

    メールチェックバターンの修正。

--- src/dataforms/validator/MailAddressValidator.java
+++ src/dataforms/validator/MailAddressValidator.java
@@ -10,6 +10,6 @@ public class MailAddressValidator extends RegexpValidator {
 	 */
 	public MailAddressValidator() {
 //		super("error.mailaddress", "^.+@.+\\..+$");
-		super("error.mailaddress", "([a-zA-Z0-9][a-zA-Z0-9_.+\\-]*)@(([a-zA-Z0-9][a-zA-Z0-9_\\-]+\\.)+[a-zA-Z]{2,6})");
+		super("error.mailaddress", "^([a-zA-Z0-9][a-zA-Z0-9_.+\\-]*)@(([a-zA-Z0-9][a-zA-Z0-9_\\-]*\\.)+[a-zA-Z]{2,6})$");
 	}
 }
