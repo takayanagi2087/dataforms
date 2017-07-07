fed92707d85ad1e95ee922e85bf27c4a5c2bc886
takayanagi <takayanagi@takayanagi-mc>
2017-07-07 10:22:53 +0900

    ユーザ情報を読み込んだ際にユーザ属性値が設定されない不具合を修正。

--- HTML/META-INF/resources/dataforms/app/field/user/UserAttributeValueField.js
+++ HTML/META-INF/resources/dataforms/app/field/user/UserAttributeValueField.js
@@ -12,6 +12,26 @@
  */
 UserAttributeValueField = createSubclass("UserAttributeValueField", {}, "EnumOptionSingleSelectField");
 
+
+/**
+ * 値の設定を行ないます。
+ * <pre>
+ * 同時にユーザ属性値の選択肢も更新します。
+ * </pre>
+ * @param {String} v 値。
+ *
+ */
+UserAttributeValueField.prototype.setValue = function(v) {
+	// この設定処理は選択肢がそろっていないので空振りします。
+	EnumOptionSingleSelectField.prototype.setValue.call(this, v);
+	// 一旦値を保持し、選択肢を取得してから設定します。
+	this.value = v;
+	var tid = this.id.replace("userAttributeValue", "userAttributeType");
+	var type = this.parent.find("#" + this.selectorEscape(tid)).val();
+	this.setUserAttributeType(type);
+}
+
+
 /**
  * ユーザ属性を設定します。
  * <pre>
@@ -25,6 +45,8 @@ UserAttributeValueField.prototype.setUserAttributeType = function(type) {
 	var opt = m.execute("type=" + type, function(opt) {
 		if (opt.status == ServerMethod.SUCCESS) {
 			thisField.setOptionList(opt.result);
+			// setValueから呼ばれた場合、保持した値を設定します。
+			thisField.get().val(thisField.value);
 		}
 	});
 };
