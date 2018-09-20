88d2e461b63723e082f282c6b39ee15f05c4ca07
takayanagi <takayanagi@takayanagi-mc>
2018-09-20 09:38:34 +0900

    LoginIdFieldのサブクラスを作るとエラーする問題を修正。

---
+++ HTML/META-INF/resources/dataforms/app/field/user/LoginIdField.js
@@ -0,0 +1,12 @@
+/**
+ * @fileOverview {@link LoginIdField}クラスを記述したファイルです。
+ */
+
+/**
+ * @class LoginIdField
+ * ユーザIDフィールドクラス。
+ * <pre>
+ * </pre>
+ * @extends TextField
+ */
+LoginIdField = createSubclass("LoginIdField", {}, "VarcharField");
