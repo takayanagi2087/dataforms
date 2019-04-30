2e07b8df1ee1cb7101a7e5d6499aae3f1dc316f0
takay <takay@takayanagi-ms>
2019-04-30 20:28:30 +0900

    ClobField.jsがなかったので追加した。

---
+++ HTML/META-INF/resources/dataforms/field/sqltype/ClobField.js
@@ -0,0 +1,13 @@
+
+/**
+ * @fileOverview {@link ClobField}クラスを記述したファイルです。
+ */
+
+/**
+ * @class ClobField
+ * Varchar型フィールドクラス。
+ * <pre>
+ * </pre>
+ * @extends TextField
+ */
+ClobField = createSubclass("ClobField", {}, "TextField");
